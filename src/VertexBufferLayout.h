faee6f4a4d46391607f906aeb6f8027fe6fac7bc
Ranjan Sengupta <ranjansemail95@gmail.com>
2020-07-04 23:10:18 +0530

    The code now crashes with a breakpoint trap

--- src/VertexBufferLayout.h
+++ src/VertexBufferLayout.h
@@ -28,32 +28,10 @@ private:
     std::vector<VertexBufferElement> m_Elements;
     unsigned int m_Stride;
 public:
-    VertexBufferLayout()
-        : m_Stride(0) {}
+    VertexBufferLayout();
     template<typename T>
     void Push(unsigned int count);
     inline const std::vector<VertexBufferElement>GetElements() const {return m_Elements;}
     inline unsigned int GetStride() const {return m_Stride;}
     
 };
-
-template <>
-void VertexBufferLayout::Push<float>(unsigned int count)
-{
-    m_Elements.push_back({GL_FLOAT, count, GL_FALSE});
-    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
-}
-
-template<>
-void VertexBufferLayout::Push<unsigned int>(unsigned int count)
-{
-    m_Elements.push_back({GL_UNSIGNED_INT, count, GL_FALSE});
-    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
-}
-
-template<>
-void VertexBufferLayout::Push<unsigned char>(unsigned int count)
-{
-    m_Elements.push_back({GL_UNSIGNED_BYTE, count, GL_TRUE});
-    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
-}
