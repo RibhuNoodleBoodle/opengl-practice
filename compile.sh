6b65d0126fdc3fc0c3f52b6b3f750f505a1bcc1e
Ranjan Sengupta <ranjansemail95@gmail.com>
2020-07-04 14:38:27 +0530

    changed compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-g++ ~/opengl-practice/src/Renderer.cpp ~/opengl-practice/src/glfwtest_R.cpp ~/opengl-practice/src/IndexBuffer.cpp ~/opengl-practice/src/VertexBuffer.cpp   -o ~/opengl-practice/openGLWindow -lglfw3 -pthread -ldl -lGLU -lGLEW -lGL -lrt -lXrandr -lXxf86vm -lXi -lXinerama -lX11
+g++ '/home/ribhu/opengl-practice/src/app.cpp' '/home/ribhu/opengl-practice/src/IndexBuffer.cpp' '/home/ribhu/opengl-practice/src/Renderer.cpp' '/home/ribhu/opengl-practice/src/VertexArray.cpp' '/home/ribhu/opengl-practice/src/VertexBuffer.cpp'  -o ~/opengl-practice/openGLWindow -lglfw3 -pthread -ldl -lGLU -lGLEW -lGL -lrt -lXrandr -lXxf86vm -lXi -lXinerama -lX11
+
